17d0ac7e2ba3da3489eac731f37b00bd31f14e52
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-05 22:02:32 -0400

    printTracks initialized

--- library.js
+++ library.js
@@ -45,16 +45,27 @@ console.log(tracksObj);
 // p02: Other Playlist - 1 tracks
 
 
+// const printPlaylists = function(obj) {
+//   let playlistName = "";
+//   let numOfTracks = 0;
+//   let results = "";
+//     for (let playlistNum in obj) {
+//       playlistName = obj[playlistNum]["name"];
+//       numOfTracks = obj[playlistNum]["tracks"].length;
+//       console.log(`${playlistNum}: ${playlistName} - ${numOfTracks} tracks`)
+//     }
+// }
+
 const printPlaylists = function(obj) {
-  let playlistName = "";
-  let numOfTracks = 0;
-  let results = "";
-    for (let playlistNum in obj) {
-      playlistName = obj[playlistNum]["name"];
-      numOfTracks = obj[playlistNum]["tracks"].length;
-      console.log(`${playlistNum}: ${playlistName} - ${numOfTracks} tracks`)
-    }
-}
+      for (let playlistNum in obj) {
+        playlistName = obj[playlistNum]["name"];
+        numOfTracks = obj[playlistNum]["tracks"].length;
+        console.log(`${playlistNum}: ${obj[playlistNum]["name"]} - ${obj[playlistNum]["tracks"].length} tracks`)
+      }
+  }
+
+printPlaylists(playlistObj);
+
 // const printPlaylists = function(obj) {
 //     let playlistName = "";
 //     let numOfTracks = 0;
@@ -75,18 +86,60 @@ const printPlaylists = function(obj) {
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 // t03: Four Thirty-Three by John Cage (Woodstock 1952)
-const printTracks = function() {
 
-}
+const printTracks = function(obj) {
+    let trackName = "";
+    let artist = "";
+    let albumName = "";
 
+      for (let trackNum in obj) {
+        trackName = obj[trackNum]["name"];
+        artistName = obj[trackNum]["artist"];
+        albumName = obj[trackNum]["album"]
+        console.log(`${trackNum}: ${trackName} by ${artistName} (${albumName}) `)
+      }
+  };
+
+  printTracks(tracksObj);
 
 // prints a list of tracks for a given playlist, using the following format:
 // p01: Coding Music - 2 tracks
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 const printPlaylist = function(playlistId) {
+trackArr = playlistObj[playlistId]["tracks"];
 
 }
+
+printPlaylist("p01");
+
+// const library = {
+//     tracks: { t01: { id: "t01",
+//                      name: "Code Monkey",
+//                      artist: "Jonathan Coulton",
+//                      album: "Thing a Week Three" },
+//               t02: { id: "t02",
+//                      name: "Model View Controller",
+//                      artist: "James Dempsey",
+//                      album: "WWDC 2003"},
+//               t03: { id: "t03",
+//                      name: "Four Thirty-Three",
+//                      artist: "John Cage",
+//                      album: "Woodstock 1952"}
+//             },
+//     playlists: { p01: { id: "p01",
+//                         name: "Coding Music",
+//                         tracks: ["t01", "t02"]
+//                       },
+//                  p02: { id: "p02",
+//                         name: "Other Playlist",
+//                         tracks: ["t03"]
+//                       }
+//                }
+//   };
+  
+
+
 
 
 // adds an existing track to an existing playlist
