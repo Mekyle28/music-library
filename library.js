a1b831d1d4f3b4b467b04f55ab26641c2a811412
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-31 11:54:08 -0400

    add addPlaylist and addTrack function

--- library.js
+++ library.js
@@ -1,34 +1,44 @@
 const library = {
-  tracks: { t01: { id: "t01",
-                   name: "Code Monkey",
-                   artist: "Jonathan Coulton",
-                   album: "Thing a Week Three" },
-            t02: { id: "t02",
-                   name: "Model View Controller",
-                   artist: "James Dempsey",
-                   album: "WWDC 2003"},
-            t03: { id: "t03",
-                   name: "Four Thirty-Three",
-                   artist: "John Cage",
-                   album: "Woodstock 1952"}
-          },
-  playlists: { p01: { id: "p01",
-                      name: "Coding Music",
-                      tracks: ["t01", "t02"]
-                    },
-               p02: { id: "p02",
-                      name: "Other Playlist",
-                      tracks: ["t03"]
-                    }
-             }
+  tracks: {
+    t01: {
+      id: "t01",
+      name: "Code Monkey",
+      artist: "Jonathan Coulton",
+      album: "Thing a Week Three"
+    },
+    t02: {
+      id: "t02",
+      name: "Model View Controller",
+      artist: "James Dempsey",
+      album: "WWDC 2003"
+    },
+    t03: {
+      id: "t03",
+      name: "Four Thirty-Three",
+      artist: "John Cage",
+      album: "Woodstock 1952"
+    }
+  },
+  playlists: {
+    p01: {
+      id: "p01",
+      name: "Coding Music",
+      tracks: ["t01", "t02"]
+    },
+    p02: {
+      id: "p02",
+      name: "Other Playlist",
+      tracks: ["t03"]
+    }
+  }
 };
 
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    }
-      return console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  };
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  return console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+};
 
 playlistObj = library.playlists;
 // console.log(playlistObj);
@@ -60,12 +70,12 @@ tracksObj = library.tracks;
 // }
 
 const printPlaylists = function(obj) {
-      for (let playlistNum in obj) {
-        playlistName = obj[playlistNum]["name"];
-        numOfTracks = obj[playlistNum]["tracks"].length;
-        console.log(`${playlistNum}: ${obj[playlistNum]["name"]} - ${obj[playlistNum]["tracks"].length} tracks`)
-      }
+  for (let playlistNum in obj) {
+    playlistName = obj[playlistNum]["name"];
+    numOfTracks = obj[playlistNum]["tracks"].length;
+    console.log(`${playlistNum}: ${obj[playlistNum]["name"]} - ${obj[playlistNum]["tracks"].length} tracks`);
   }
+}
 
 printPlaylists(playlistObj);
 
@@ -90,17 +100,17 @@ printPlaylists(playlistObj);
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 // t03: Four Thirty-Three by John Cage (Woodstock 1952)
 
-const printTracks = function(obj) {
-  
-      for (let trackNum in obj) {
-        trackName = obj[trackNum]["name"];
-        artistName = obj[trackNum]["artist"];
-        albumName = obj[trackNum]["album"]
-        console.log(`${trackNum}: ${trackName} by ${artistName} (${albumName}) `)
-      }
-  };
+const printTracks = function (obj) {
 
-  printTracks(tracksObj);
+  for (let trackNum in obj) {
+    trackName = obj[trackNum]["name"];
+    artistName = obj[trackNum]["artist"];
+    albumName = obj[trackNum]["album"]
+    console.log(`${trackNum}: ${trackName} by ${artistName} (${albumName}) `)
+  }
+};
+
+printTracks(tracksObj);
 
 // prints a list of tracks for a given playlist, using the following format:
 // p01: Coding Music - 2 tracks
@@ -112,13 +122,13 @@ const printTracks = function(obj) {
 
 
 const printPlaylist = function(playlistId, libObj) {
-trackArr = libObj["playlists"][playlistId]["tracks"];
-// print playlist info
-playlistName = libObj["playlists"][playlistId]["name"];
-numOfTracks = libObj["playlists"][playlistId]["tracks"].length;
-console.log(`${playlistId}: ${playlistName} - ${numOfTracks} tracks`);
-//loop through track array to get song info
-for (let trackNum of trackArr) {
+  trackArr = libObj["playlists"][playlistId]["tracks"];
+  // print playlist info
+  playlistName = libObj["playlists"][playlistId]["name"];
+  numOfTracks = libObj["playlists"][playlistId]["tracks"].length;
+  console.log(`${playlistId}: ${playlistName} - ${numOfTracks} tracks`);
+  //loop through track array to get song info
+  for (let trackNum of trackArr) {
     trackName = libObj["tracks"][trackNum]["name"];
     artistName = libObj["tracks"][trackNum]["artist"];
     albumName = libObj["tracks"][trackNum]["album"];
@@ -131,37 +141,53 @@ printPlaylist("p01", library);
 
 
 // adds an existing track to an existing playlist
-const addTrackToPlaylist = function(trackId, playlistId) {
-addTrackObj = libObj["tracks"][trackId]
-console.log(addTrackObj);
-}
+const addTrackToPlaylist = function(trackId, playlistId, libObj) {
+  libObj.playlists[playlistId].tracks.push(trackId);
+};
 
-addTrackToPlaylist()
+addTrackToPlaylist("t03", "p01", library);
 
 // generates a unique id
 // (already implemented: use this for addTrack and addPlaylist)
-const generateUid = function() {
+const generateUid = function () {
   return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-}
+};
 
 
 // adds a track to the library
-const addTrack = function(name, artist, album) {
+const addTrack = function(name, artist, album, libObj) {
+  const newTrackNum = generateUid();
+  const newTrackobj = {
+    id: newTrackNum,
+    name,
+    artist,
+    album};
+  console.log(newTrackNum);
 
-}
+  libObj.tracks[newTrackNum] = newTrackobj;
+};
+
+addTrack('bob', 'bobbbb', 'yeabob', library);
+
 
 
 // adds a playlist to the library
-const addPlaylist = function(name) {
+const addPlaylist = function (name, libObj) {
+  const newplaylistNum = generateUid();
+  libObj.playlists[newplaylistNum] = {
+    id: newplaylistNum,
+    name,
+    tracks: [],
+  };
+  return libObj;
+};
 
-}
-
-
+console.log("add playlist", addPlaylist('coding tunes', library));
 // STRETCH:
 // given a query string string, prints a list of tracks
 // where the name, artist or album contains the query string (case insensitive)
 // tip: use "string".search("tri") 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
-const printSearchResults = function(query) {
+const printSearchResults = function (query) {
 
 }
